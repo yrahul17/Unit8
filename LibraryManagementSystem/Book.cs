0f7a828c927660e95c98a928ff532ab2d720c2ea
Rahul Yadav <rky6008@rit.edu>
2024-02-07 22:50:02 -0500

    Completed

---
+++ LibraryManagementSystem/Book.cs
@@ -0,0 +1,100 @@
+﻿/**========================================
+Professor Kemoy Campbell
+Author: Rahul Yadav
+NACA.121
+February 7, 2024
+==========================================**/
+
+
+using System.ComponentModel.DataAnnotations;
+
+public class Book
+{
+    //set up attributes
+    public string title;
+    public string author;
+    public int pages;
+
+    private int pagesRead;
+
+    //Default constructor
+    public Book()
+    {
+        title = "Unspecified";
+        author = "N/A";
+        pages = 0;
+        pagesRead = 0;
+    }
+
+    //Paramterized constructor
+    public Book(string titleName, string authorName, int pageNumber)
+    {
+        title = titleName;
+        author = authorName;
+        pages = pageNumber;
+        pagesRead = 0;
+    }
+
+    //ToString override
+    public override string ToString()
+    {
+        return $"Book 1\nTitle: {title}\nAuthor: {author}\nPages: {pages}\nPagesRead: {pagesRead}";
+    }
+
+    private bool lsBookValid()
+    {
+        return pages > 0;
+    }
+
+    public string ReadPage()
+    {
+        if (!lsBookValid())
+        {
+            return "Invalid Book. Unable to read";
+        }
+
+        else if (pagesRead >= pages)
+        {
+            return "This book has been read entirely.";
+        }
+
+        else if (pagesRead == pages - 1)
+        {
+            pagesRead++;
+            return $"You read one page out of a total {pages} page(s), completing the entire book.";
+        }
+        else
+        {
+            pagesRead++;
+            int remainingPages = pages - pagesRead;
+            return $"You read one page and have {remainingPages} left to read out of a total of {pages}.";
+        }
+    }
+
+    public string ReadPage(int pagesToRead)
+    {
+        if (!lsBookValid())
+        {
+            return "Invalid Book. Unable to read";
+        }
+         else if (pagesToRead <= 0 || pagesToRead > pages - pagesRead)
+        {
+            return "Invalid number of pages to read";
+        }
+        else
+        {
+            pagesToRead += pagesToRead;
+
+            if (pagesToRead >= pages)
+            {
+                return "You read this book entirely";
+            }
+            else
+            {
+                int remainingPages = pages - pagesRead;
+                return $"You read {pagesToRead} page(s) and have {remainingPages} page(s) left to read out of {pages}";
+            }
+        }
+    }
+
+}
---
+++ LibraryManagementSystem/Program.cs
@@ -0,0 +1,68 @@
+﻿//Problem 1 and Problem 2
+// //Creating book1 using the default constructor
+// Book book1 = new Book();
+// //Invoke toString method
+// Console.WriteLine(book1.ToString());
+// Console.WriteLine();
+
+// //Create book2 using parameterized constructor
+// Book book2 = new Book("Hamlet", "Williamn Shakespeare", 289);
+// Console.WriteLine("Book 2");
+// Console.WriteLine($"Title: {book2.title}\nAuthor:{book2.author}\nPages: {book2.pages}");
+
+// // Create book3 with page 1
+// Book book3 = new Book("Random Book", "Random Author", 1);
+
+// //Invoke readpage method for book1, book2 and book3
+// Console.WriteLine("\nAttempting to Read Book 1:");
+// Console.WriteLine(book1.ReadPage());
+// Console.WriteLine();
+
+
+// Console.WriteLine("\nAttempting to Read Book 2:");
+// Console.WriteLine(book2.ReadPage());
+// Console.WriteLine();
+
+// //Read book3 two times
+// Console.WriteLine("\nAttempting to Read Book 3:");
+// Console.WriteLine(book3.ReadPage());
+// Console.WriteLine();
+
+// Console.WriteLine("Attempting to Read Book 3 again:");
+// Console.WriteLine(book3.ReadPage());
+
+
+// Problem3
+
+Book book1 = new();
+Book book2 = new("Hamlet", "William Shakespeare", 289);
+Book book3 = new("Some Book", "Some Author", 1);
+
+//Attempt to Read Book 1 (read one page)
+Console.WriteLine(book1.ReadPage());
+Console.WriteLine();
+
+//Attempting to Read Book 2 (read one page)
+Console.WriteLine(book2.ReadPage());
+Console.WriteLine();
+
+//Attempting to Read Book 2 (read -1 page)
+Console.WriteLine(book2.ReadPage(-1));
+Console.WriteLine();
+
+//Attempting to Read Book 2 (read 8 page)
+Console.WriteLine(book2.ReadPage(8));
+Console.WriteLine();
+
+// Attempting to Read 289 pages of Book 2
+Console.WriteLine(book2.ReadPage(289));
+Console.WriteLine();
+
+// Attempting to read 280 pages of Book 2
+Console.WriteLine(book2.ReadPage(280));
+Console.WriteLine();
+
+// Attempting to Read 1 page of Book 3
+Console.WriteLine(book3.ReadPage(1));
+Console.WriteLine();
+
